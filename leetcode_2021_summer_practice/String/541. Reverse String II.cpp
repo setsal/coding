c864c12ade11e0e0acc6ec5e93095387291acdba
setsal Lan <contact@setsal.dev>
2021-08-24 23:14:12 +0800

    update

---
+++ leetcode_2021_summer_practice/String/541. Reverse String II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2*k)) {
+            if ( i + k <=  s.size() ) {
+                reverse(s.begin() + i, s.begin() + i + k);  
+                continue;
+            }
+            reverse(s.begin() + i, s.begin() + s.size());
+        }
+        return s;
+    }
+};
